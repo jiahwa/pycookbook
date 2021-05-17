55a3d7f79218a31bbd9aea068675fe8dc8a469f7
yujahua <yujahua@163.com>
2021-05-18 00:46:11 +0800

    chore(todolist):update askme.py move / right to left

--- todo/askme.py
+++ todo/askme.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # ask me and make choice
 print('Press Ctrl-C to quit.')
-stat = '*-* ASK ME'
+stat = '** ASK ME **'
+count = 1
 try:
-    while True:
-        print(stat)
-        print('\b' * len(stat), end='', flush=True)
+    while count-1<len(stat):
+        # back keyboard count times, count ++ until len(stat), end is from count pos to the last pos
+        startPos = len(stat) - count
+        endPos = len(stat)
+        count = count + 1
+        # if(count == endPos):
+        #     count = 1
+        print('\b' * 12, end=stat[0:startPos] + '/' + stat[startPos+1:endPos], flush=True)
 except KeyboardInterrupt:
     print('\nDone.')
